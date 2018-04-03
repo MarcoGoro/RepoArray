cb4c769a518002eddd8c12330558199a3031d880
Utente <Utente@UTENTE-PC>
2018-04-03 11:30:10 +0200

    aggiunta metodo e test

---
+++ SolutionArray/LibraryCollezione.Test/VettoreTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryCollezione;
+
+namespace LibraryCollezione.Test
+{
+    [TestClass]
+    public class VettoreTest
+    {
+        [TestMethod]
+        public void TestArrayOrdinamento()
+        {
+            int[] v1 = new int[] { 11, 22, 13, 4 };
+            int[] v2 = new int[] { 4, 11, 3, 22 };
+            int[] vettoreOrdinato = Vettore.Ordinamento(v1);
+            CollectionAssert.AreEqual(v2, vettoreOrdinato);
+        }
+    }
+}
